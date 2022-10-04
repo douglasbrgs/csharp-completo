13cca6d7bc819636d96a098da3b40b160aba0d0b
Douglas Borges <douglasborges19@gmail.com>
2022-10-03 21:30:22 -0300

    Acrescenta enumeracoes-composicao

---
+++ 9-enumeracoes-composicao/Enumeracao1/Enumeracao1/Entities/Order.cs
@@ -0,0 +1,18 @@
+﻿using Enumeracao1.Entities.Enums;
+
+namespace Enumeracao1.Entities
+{
+    internal class Order
+    {
+        public int Id { get; set; }
+
+        public DateTime Moment { get; set; }
+
+        public OrderStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Moment}, {Status}";
+        }
+    }
+}
---
+++ 9-enumeracoes-composicao/Enumeracao1/Enumeracao1/Program.cs
@@ -0,0 +1,25 @@
+﻿using Enumeracao1.Entities;
+using Enumeracao1.Entities.Enums;
+
+namespace Enumeracao1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order {
+                Id = 1234,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txtStatus = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txtStatus);
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+        }
+    }
+}
