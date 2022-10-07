4cfcf6d1bed6fe968f5efeaee6e8c34c9878d8e1
Douglas Borges <douglasborges19@gmail.com>
2022-10-07 13:19:31 -0300

    Acrescenta 10-Heranca

---
+++ 10-heranca/Heranca1/Heranca1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Heranca1.Entities;
+
+namespace Heranca1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessAccount account = new BusinessAccount(1234, "Bob Brown", 150.00, 200.00);
+
+            Console.WriteLine(account.Balance);
+
+            //Encapsulamento
+            //account.Balance = 300.00;
+
+        }
+    }
+}
