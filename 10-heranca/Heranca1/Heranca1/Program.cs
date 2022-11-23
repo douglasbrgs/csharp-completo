6ead1014f9125fce733dd4ab1efa6aab00cc4a73
Douglas Borges <douglasborges19@gmail.com>
2022-11-22 23:09:54 -0300

    Update Program

--- 10-heranca/Heranca1/Heranca1/Program.cs
+++ 10-heranca/Heranca1/Heranca1/Program.cs
@@ -6,13 +6,13 @@ namespace Heranca1
     {
         static void Main(string[] args)
         {
-            BusinessAccount account = new BusinessAccount(1234, "Bob Brown", 150.00, 200.00);
+            BusinessAccount account = new BusinessAccount(1234, "Bob Brown", 150.00, 500.00);
+
+            account.Deposit(150.00);
+            account.WithDraw(100.00);
+            account.Loan(500.00);
 
             Console.WriteLine(account.Balance);
-
-            //Encapsulamento
-            //account.Balance = 300.00;
-
         }
     }
 }
