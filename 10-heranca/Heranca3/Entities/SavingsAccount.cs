07044b4293a418f08c43cadc1368a89b5a34f50f
Douglas Borges <douglasborges19@gmail.com>
2022-11-23 21:59:56 -0300

    Add Heranca3

---
+++ 10-heranca/Heranca3/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace Heranca3.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance = Balance * InterestRate;
+        }
+
+        //"override" indica que esta sobrescrevendo um método da superclasse
+        public override void WithDraw(double amount)
+        {
+            //"base" chama a implementação do método na superclasse
+            base.WithDraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ 10-heranca/Heranca3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Heranca3.Entities;
+
+namespace Heranca3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500.0);
+            Account acc2 = new SavingsAccount(1002, "Bob", 500.0, 0.01);
+
+            acc1.WithDraw(10.0);
+            acc2.WithDraw(10.0);
+
+            //saque + 5.0 (taxa account)
+            Console.WriteLine(acc1.Balance);
+
+            // saque + 5.0 (taxa account) + 2.0 (taxa savings)
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
